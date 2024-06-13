9cf80528fcb739e79d213a40f04e6011d6badc21
Keren Cohen <keren.c91@gmail.com>
2024-06-13 19:09:31 +0300

    Update noise_ann_script

--- run_noise_annealing.sh
+++ run_noise_annealing.sh
@@ -1,6 +1,6 @@
 #noise annealing
-python main.py --audio "audio_files/dog.wav" --input_length 10 --workspace "results/noise_ann/trial_audioToken_noise_ann_1e-2_dog" --hf_key CompVis/stable-diffusion-v1-4 -O --noise_annealing 11 --iters 20000;
-python main.py --audio "audio_files/dog.wav" --input_length 10 --workspace "results/noise_ann/trial_audioToken_noise_ann_1e-2_dog" --hf_key CompVis/stable-diffusion-v1-4 -O --noise_annealing 1e-1 --iters 20000;
+python main.py --audio "audio_files/dog.wav" --input_length 10 --workspace "results/noise_ann/trial_audioToken_noise_ann_11_dog" --hf_key CompVis/stable-diffusion-v1-4 -O --noise_annealing 11 --iters 20000;
+python main.py --audio "audio_files/dog.wav" --input_length 10 --workspace "results/noise_ann/trial_audioToken_noise_ann_1e-1_dog" --hf_key CompVis/stable-diffusion-v1-4 -O --noise_annealing 1e-1 --iters 20000;
 python main.py --audio "audio_files/dog.wav" --input_length 10 --workspace "results/noise_ann/trial_audioToken_noise_ann_1e-2_dog" --hf_key CompVis/stable-diffusion-v1-4 -O --noise_annealing 1e-2 --iters 20000;
 python main.py --audio "audio_files/dog.wav" --input_length 10 --workspace "results/noise_ann/trial_audioToken_noise_ann_1e-3_dog" --hf_key CompVis/stable-diffusion-v1-4 -O --noise_annealing 1e-3 --iters 20000;
 python main.py --audio "audio_files/dog.wav" --input_length 10 --workspace "results/noise_ann/trial_audioToken_noise_ann_1e-4_dog" --hf_key CompVis/stable-diffusion-v1-4 -O --noise_annealing 1e-4 --iters 20000;
